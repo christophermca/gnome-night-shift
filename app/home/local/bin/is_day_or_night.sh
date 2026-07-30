53b39918eaffe975c88b4f28f82290aba94b77f2
Christopher MCAdams <mca.christopher@gmail.com>
2026-07-30 16:53:06 -0400

    Chore: Clean up logic in is_day_or_night.sh
    fix: Variables
    fix: includes logs

--- app/home/local/bin/is_day_or_night.sh
+++ app/home/local/bin/is_day_or_night.sh
@@ -4,8 +4,7 @@ source /usr/lib/gnome-night-shift/shared-variables.sh
 
 
 is_day_or_night() {
-  start_stop="$(cat ${STATE_FOLDER}/times)"
-  IFS="," read -r start stop <<< "$start_stop"
+  IFS="," read -r stop start <<< "$(cat ${STATE_FOLDER}/times)"
   current_time=`date '+%H:%M'`
   echo "---" $current_time $start $stop
 
@@ -17,16 +16,24 @@ is_day_or_night() {
     DAY_NIGHT='night'
   fi
 
+  echo "its ${DAY_NIGHT}time"
+
   save_configuration() {
+    echo "save_configuration: is cache up-to-date?"
     local -r day_night_mode=$(cat $IS_DAY_OR_NIGHT)
 
     if [[ -n $DAY_NIGHT && $day_night_mode != $DAY_NIGHT ]]; then
+
       if [[ ! -f  $IS_DAY_OR_NIGHT ]]; then
         touch $IS_DAY_OR_NIGHT
       fi
 
+      echo "Updating cache"
+      #save to file
       echo $DAY_NIGHT > $IS_DAY_OR_NIGHT
+
       export DAY_NIGHT=$DAY_NIGHT
+
     else
       # does nothing
       echo "The current state is the same"
--- app/home/local/gnome-night-shift/shared-variables.sh
+++ app/home/local/gnome-night-shift/shared-variables.sh
@@ -13,5 +13,5 @@ RDSHFT_DAY="daytime"
 
 NIGHT_MODE="night"
 DAY_MODE="day"
-IS_DAY_OR_NIGHT="/home/${USER}/.local/state/gnome-night-shift/is-day-or-night"
-
+STATE_FOLDER="/home/${USER}/.local/state/gnome-night-shift"
+IS_DAY_OR_NIGHT="${STATE_FOLDER}/is-day-or-night"
